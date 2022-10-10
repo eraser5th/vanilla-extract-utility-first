97dded457625a784d7ea0ff2ddcacaf3167c49f2
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-10-11 01:26:12 +0900

    Support for changes in pxUnits

--- src/styles/utils/spacing/margin.css.ts
+++ src/styles/utils/spacing/margin.css.ts
@@ -1,118 +1,13 @@
 import { style } from "@vanilla-extract/css";
-import { pxUnits } from "../../constants/pxUnits";
+import { PxUnit, pxUnitKeys } from "../../constants/pxUnits";
 
-enum Margin {
-  px0 = "0px",
-  px1 = "1px",
-  px2 = "2px",
-  px4 = "4px",
-  px6 = "6px",
-  px8 = "8px",
-  px10 = "10px",
-  px12 = "12px",
-  px14 = "14px",
-  px16 = "16px",
-  px20 = "20px",
-  px24 = "24px",
-  px28 = "28px",
-  px32 = "32px",
-  px36 = "36px",
-  px40 = "40px",
-  px44 = "44px",
-  px48 = "48px",
-  px52 = "52px",
-  px56 = "56px",
-  px60 = "60px",
-  px64 = "64px",
-  px68 = "68px",
-  px72 = "72px",
-  px76 = "76px",
-  px80 = "80px",
-  px84 = "84px",
-  px88 = "88px",
-  px92 = "92px",
-  px96 = "96px",
-  px100 = "100px",
-  px104 = "104px",
-  px108 = "108px",
-  px112 = "112px",
-  px116 = "116px",
-  px120 = "120px",
-  px124 = "124px",
-  px128 = "128px",
-  px132 = "132px",
-  px136 = "136px",
-  px140 = "140px",
-  px144 = "144px",
-  px148 = "148px",
-  px152 = "152px",
-  px156 = "156px",
-  px160 = "160px",
-  px164 = "164px",
-  px168 = "168px",
-  px172 = "172px",
-  px176 = "176px",
-  px180 = "180px",
-  px184 = "184px",
-  px188 = "188px",
-  px192 = "192px",
-  px196 = "196px",
-  px200 = "200px",
-  px204 = "204px",
-  px208 = "208px",
-  px212 = "212px",
-  px216 = "216px",
-  px220 = "220px",
-  px224 = "224px",
-  px228 = "228px",
-  px232 = "232px",
-  px236 = "236px",
-  px240 = "240px",
-  px244 = "244px",
-  px248 = "248px",
-  px252 = "252px",
-  px256 = "256px",
-  px260 = "260px",
-  px264 = "264px",
-  px268 = "268px",
-  px272 = "272px",
-  px276 = "276px",
-  px280 = "280px",
-  px284 = "284px",
-  px288 = "288px",
-  px292 = "292px",
-  px296 = "296px",
-  px300 = "300px",
-  px304 = "304px",
-  px308 = "308px",
-  px312 = "312px",
-  px316 = "316px",
-  px320 = "320px",
-  px324 = "324px",
-  px328 = "328px",
-  px332 = "332px",
-  px336 = "336px",
-  px340 = "340px",
-  px344 = "344px",
-  px348 = "348px",
-  px352 = "352px",
-  px356 = "356px",
-  px360 = "360px",
-  px364 = "364px",
-  px368 = "368px",
-  px372 = "372px",
-  px376 = "376px",
-  px380 = "380px",
-  px384 = "384px",
-}
-
-type MarginStyles = Record<`px${typeof pxUnits[number]}`, string>;
+type MarginStyles = Record<typeof pxUnitKeys[number], string>;
 
 const partialM: Partial<MarginStyles> = {};
 
-pxUnits.forEach((pxUnit) => {
-  partialM[`px${pxUnit}`] = style({
-    margin: pxUnit + "px",
+pxUnitKeys.forEach((pxUnitKey) => {
+  partialM[pxUnitKey] = style({
+    margin: PxUnit[pxUnitKey],
   });
 });
 
@@ -120,9 +15,9 @@ export const m: MarginStyles = partialM as MarginStyles;
 
 const partialMl: Partial<MarginStyles> = {};
 
-pxUnits.forEach((pxUnit) => {
-  partialMl[`px${pxUnit}`] = style({
-    marginLeft: pxUnit + "px",
+pxUnitKeys.forEach((pxUnitKey) => {
+  partialMl[pxUnitKey] = style({
+    marginLeft: PxUnit[pxUnitKey],
   });
 });
 
@@ -130,9 +25,9 @@ export const ml: MarginStyles = partialMl as MarginStyles;
 
 const partialMr: Partial<MarginStyles> = {};
 
-pxUnits.forEach((pxUnit) => {
-  partialMr[`px${pxUnit}`] = style({
-    marginRight: pxUnit + "px",
+pxUnitKeys.forEach((pxUnitKey) => {
+  partialMr[pxUnitKey] = style({
+    marginRight: PxUnit[pxUnitKey],
   });
 });
 
@@ -140,9 +35,9 @@ export const mr: MarginStyles = partialMr as MarginStyles;
 
 const partialMt: Partial<MarginStyles> = {};
 
-pxUnits.forEach((pxUnit) => {
-  partialMt[`px${pxUnit}`] = style({
-    marginTop: pxUnit + "px",
+pxUnitKeys.forEach((pxUnitKey) => {
+  partialMt[pxUnitKey] = style({
+    marginTop: PxUnit[pxUnitKey],
   });
 });
 
@@ -150,34 +45,30 @@ export const mt: MarginStyles = partialMt as MarginStyles;
 
 const partialMb: Partial<MarginStyles> = {};
 
-pxUnits.forEach((pxUnit) => {
-  partialMb[`px${pxUnit}`] = style({
-    marginBottom: pxUnit + "px",
+pxUnitKeys.forEach((pxUnitKey) => {
+  partialMb[pxUnitKey] = style({
+    marginBottom: PxUnit[pxUnitKey],
   });
 });
 
 export const mb: MarginStyles = partialMb as MarginStyles;
 
-export const mx: MarginStyles = pxUnits
-  .map((pxUnit) => {
-    const key: keyof MarginStyles = `px${pxUnit}`;
-    const a = {
-      [key]: style([ml[key], mr[key]]),
+export const mx: MarginStyles = pxUnitKeys
+  .map((pxUnitKey) => {
+    return {
+      [pxUnitKey]: style([ml[pxUnitKey], mr[pxUnitKey]]),
     };
-    return a;
   })
   .reduce((a, b) => ({
     ...a,
     ...b,
   })) as MarginStyles;
 
-export const my: MarginStyles = pxUnits
-  .map((pxUnit) => {
-    const key: keyof MarginStyles = `px${pxUnit}`;
-    const a = {
-      [key]: style([mt[key], mb[key]]),
+export const my: MarginStyles = pxUnitKeys
+  .map((pxUnitKey) => {
+    return {
+      [pxUnitKey]: style([mt[pxUnitKey], mb[pxUnitKey]]),
     };
-    return a;
   })
   .reduce((a, b) => ({
     ...a,
@@ -185,11 +76,11 @@ export const my: MarginStyles = pxUnits
   })) as MarginStyles;
 
 export const marginProperties = {
-  margin: Margin,
-  marginLeft: Margin,
-  marginRight: Margin,
-  marginTop: Margin,
-  marginBottom: Margin,
+  margin: PxUnit,
+  marginLeft: PxUnit,
+  marginRight: PxUnit,
+  marginTop: PxUnit,
+  marginBottom: PxUnit,
 };
 
 export const marginShorthands = {
--- src/styles/utils/spacing/padding.css.ts
+++ src/styles/utils/spacing/padding.css.ts
@@ -1,118 +1,13 @@
 import { style } from "@vanilla-extract/css";
-import { pxUnits } from "../../constants/pxUnits";
+import { PxUnit, pxUnitKeys } from "../../constants/pxUnits";
 
-enum Padding {
-  px0 = "0px",
-  px1 = "1px",
-  px2 = "2px",
-  px4 = "4px",
-  px6 = "6px",
-  px8 = "8px",
-  px10 = "10px",
-  px12 = "12px",
-  px14 = "14px",
-  px16 = "16px",
-  px20 = "20px",
-  px24 = "24px",
-  px28 = "28px",
-  px32 = "32px",
-  px36 = "36px",
-  px40 = "40px",
-  px44 = "44px",
-  px48 = "48px",
-  px52 = "52px",
-  px56 = "56px",
-  px60 = "60px",
-  px64 = "64px",
-  px68 = "68px",
-  px72 = "72px",
-  px76 = "76px",
-  px80 = "80px",
-  px84 = "84px",
-  px88 = "88px",
-  px92 = "92px",
-  px96 = "96px",
-  px100 = "100px",
-  px104 = "104px",
-  px108 = "108px",
-  px112 = "112px",
-  px116 = "116px",
-  px120 = "120px",
-  px124 = "124px",
-  px128 = "128px",
-  px132 = "132px",
-  px136 = "136px",
-  px140 = "140px",
-  px144 = "144px",
-  px148 = "148px",
-  px152 = "152px",
-  px156 = "156px",
-  px160 = "160px",
-  px164 = "164px",
-  px168 = "168px",
-  px172 = "172px",
-  px176 = "176px",
-  px180 = "180px",
-  px184 = "184px",
-  px188 = "188px",
-  px192 = "192px",
-  px196 = "196px",
-  px200 = "200px",
-  px204 = "204px",
-  px208 = "208px",
-  px212 = "212px",
-  px216 = "216px",
-  px220 = "220px",
-  px224 = "224px",
-  px228 = "228px",
-  px232 = "232px",
-  px236 = "236px",
-  px240 = "240px",
-  px244 = "244px",
-  px248 = "248px",
-  px252 = "252px",
-  px256 = "256px",
-  px260 = "260px",
-  px264 = "264px",
-  px268 = "268px",
-  px272 = "272px",
-  px276 = "276px",
-  px280 = "280px",
-  px284 = "284px",
-  px288 = "288px",
-  px292 = "292px",
-  px296 = "296px",
-  px300 = "300px",
-  px304 = "304px",
-  px308 = "308px",
-  px312 = "312px",
-  px316 = "316px",
-  px320 = "320px",
-  px324 = "324px",
-  px328 = "328px",
-  px332 = "332px",
-  px336 = "336px",
-  px340 = "340px",
-  px344 = "344px",
-  px348 = "348px",
-  px352 = "352px",
-  px356 = "356px",
-  px360 = "360px",
-  px364 = "364px",
-  px368 = "368px",
-  px372 = "372px",
-  px376 = "376px",
-  px380 = "380px",
-  px384 = "384px",
-}
-
-type PaddingStyles = Record<`px${typeof pxUnits[number]}`, string>;
+type PaddingStyles = Record<typeof pxUnitKeys[number], string>;
 
 const partialP: Partial<PaddingStyles> = {};
 
-pxUnits.forEach((pxUnit) => {
-  partialP[`px${pxUnit}`] = style({
-    padding: pxUnit + "px",
+pxUnitKeys.forEach((pxUnitKey) => {
+  partialP[pxUnitKey] = style({
+    padding: PxUnit[pxUnitKey],
   });
 });
 
@@ -120,9 +15,9 @@ export const p: PaddingStyles = partialP as PaddingStyles;
 
 const partialPl: Partial<PaddingStyles> = {};
 
-pxUnits.forEach((pxUnit) => {
-  partialPl[`px${pxUnit}`] = style({
-    paddingLeft: pxUnit + "px",
+pxUnitKeys.forEach((pxUnitKey) => {
+  partialPl[pxUnitKey] = style({
+    paddingLeft: PxUnit[pxUnitKey],
   });
 });
 
@@ -130,9 +25,9 @@ export const pl: PaddingStyles = partialPl as PaddingStyles;
 
 const partialPr: Partial<PaddingStyles> = {};
 
-pxUnits.forEach((pxUnit) => {
-  partialPr[`px${pxUnit}`] = style({
-    paddingRight: pxUnit + "px",
+pxUnitKeys.forEach((pxUnitKey) => {
+  partialPr[pxUnitKey] = style({
+    paddingRight: PxUnit[pxUnitKey],
   });
 });
 
@@ -140,9 +35,9 @@ export const pr: PaddingStyles = partialPr as PaddingStyles;
 
 const partialPt: Partial<PaddingStyles> = {};
 
-pxUnits.forEach((pxUnit) => {
-  partialPt[`px${pxUnit}`] = style({
-    paddingTop: pxUnit + "px",
+pxUnitKeys.forEach((pxUnitKey) => {
+  partialPt[pxUnitKey] = style({
+    paddingTop: PxUnit[pxUnitKey],
   });
 });
 
@@ -150,19 +45,18 @@ export const pt: PaddingStyles = partialPt as PaddingStyles;
 
 const partialPb: Partial<PaddingStyles> = {};
 
-pxUnits.forEach((pxUnit) => {
-  partialPb[`px${pxUnit}`] = style({
-    paddingBottom: pxUnit + "px",
+pxUnitKeys.forEach((pxUnitKey) => {
+  partialPb[pxUnitKey] = style({
+    paddingBottom: PxUnit[pxUnitKey],
   });
 });
 
 export const pb: PaddingStyles = partialPb as PaddingStyles;
 
-export const px: PaddingStyles = pxUnits
-  .map((pxUnit) => {
-    const key: keyof PaddingStyles = `px${pxUnit}`;
+export const px: PaddingStyles = pxUnitKeys
+  .map((pxUnitKey) => {
     const a = {
-      [key]: style([pl[key], pr[key]]),
+      [pxUnitKey]: style([pl[pxUnitKey], pr[pxUnitKey]]),
     };
     return a;
   })
@@ -171,11 +65,10 @@ export const px: PaddingStyles = pxUnits
     ...b,
   })) as PaddingStyles;
 
-export const py: PaddingStyles = pxUnits
-  .map((pxUnit) => {
-    const key: keyof PaddingStyles = `px${pxUnit}`;
+export const py: PaddingStyles = pxUnitKeys
+  .map((pxUnitKey) => {
     const a = {
-      [key]: style([pt[key], pb[key]]),
+      [pxUnitKey]: style([pt[pxUnitKey], pb[pxUnitKey]]),
     };
     return a;
   })
@@ -185,11 +78,11 @@ export const py: PaddingStyles = pxUnits
   })) as PaddingStyles;
 
 export const paddingProperties = {
-  padding: Padding,
-  paddingLeft: Padding,
-  paddingRight: Padding,
-  paddingTop: Padding,
-  paddingBottom: Padding,
+  padding: PxUnit,
+  paddingLeft: PxUnit,
+  paddingRight: PxUnit,
+  paddingTop: PxUnit,
+  paddingBottom: PxUnit,
 };
 
 export const paddingShorthands = {
