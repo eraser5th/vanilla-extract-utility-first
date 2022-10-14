fa422e0d6fa703a9ee06b89f19b38abf431e04c1
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-10-15 05:29:23 +0900

    use components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { app } from "./App.css";
+import { Button } from "./components/Button";
+import { Card } from "./components/Card";
 import { className } from "./styles/lib";
 import { hover } from "./styles/splinkles";
 import { bg, box, container, px, py } from "./styles/utils";
@@ -18,6 +20,12 @@ export const App: React.FC = () => {
       >
         {"ahoy".repeat(100)}
       </p>
+      <Card title="hi!">ahoy</Card>
+      <Card title="hi!" sectioned>
+        ahoy
+      </Card>
+      <Button label="click me!" />
+      <Button label="click me!" fill />
     </div>
   );
 };
